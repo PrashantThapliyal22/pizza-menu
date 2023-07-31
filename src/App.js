78eaa3de851f30d0ea996f1e7f7787ef367604f8
Prashant <thapliyalprashant285@gmail.com>
2023-07-31 14:10:25 -0700

    add some functionality

--- src/App.js
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div>
-      <h1>This Is brand</h1>
-      <button> click here</button>
-    </div>
-  );
-}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+function App() {
+  return (
+    <div>
+      <Header />
+      <Pizza />
+      <Pizza />
+      <Pizza />
+      <Footer />
+    </div>
+  );
+}
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function Header() {
+  return <h2> Fast react pizza co.</h2>;
+}
+
+function Footer() {
+  return (
+    <div>
+      <h4>{new Date().toLocaleDateString()} We are open </h4>
+    </div>
+  );
+}
+function Pizza() {
+  return (
+    <div>
+      <h2>Pizza</h2>
+      <img src="/pizzas/pizza5.jpg" alt="spinach" />
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
